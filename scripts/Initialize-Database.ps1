0fa31260535cea17a0e1abf41b2e765a47aa0d0b
dsajiz <sa@az.com>
2024-09-14 13:03:26 +1000

    提交说明

--- scripts/Initialize-Database.ps1
+++ scripts/Initialize-Database.ps1
@@ -1,5 +1,5 @@
-# # get the directory where the script is located (rather than where the user is running it from)
 $ScriptDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 Remove-Item -Path "$ScriptDir\..\database.db" -ErrorAction SilentlyContinue
-Get-Content "$ScriptDir\..\database.sql" | sqlite3 "$ScriptDir\..\database.db"
+
+Get-Content "$ScriptDir\..\database.sql" | & "C:\Users\jeffr\Downloads\sqlite-tools-win-x64-3460100\sqlite3.exe" "$ScriptDir\..\database.db"
