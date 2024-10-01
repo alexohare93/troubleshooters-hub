912e6f21ecffff7cb915656abf577ef9d1a92bbe
alexohare93 <109781158+alexohare93@users.noreply.github.com>
2024-10-02 09:22:15 +1000

    Add in a search method

--- src/main/java/hub/troubleshooters/soundlink/core/events/services/EventService.java
+++ src/main/java/hub/troubleshooters/soundlink/core/events/services/EventService.java
@@ -2,8 +2,8 @@ package hub.troubleshooters.soundlink.core.events.services;
 
 import hub.troubleshooters.soundlink.core.events.models.CreateEventModel;
 import hub.troubleshooters.soundlink.core.validation.ValidationResult;
-import hub.troubleshooters.soundlink.data.models.SearchEvent;
-import hub.troubleshooters.soundlink.data.factories.SearchEventFactory;
+import hub.troubleshooters.soundlink.core.events.models.SearchEventModel;
+import hub.troubleshooters.soundlink.data.models.Event;
 
 import java.util.List;
 import java.sql.SQLException;
@@ -15,6 +15,9 @@ public interface EventService {
     ValidationResult createEvent(CreateEventModel model);
 
     List<Event> getUserCommunityEvents(int userId) throws SQLException;
+    
     List<Event> getPublicCommunityEvents(int userId) throws SQLException;
+
+    List<Event> search(SearchEventModel searchModel) throws SQLException;
 }
 
