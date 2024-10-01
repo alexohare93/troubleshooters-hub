20adf8adb6b846454d108141a5d192f2464492c6
alexohare93 <109781158+alexohare93@users.noreply.github.com>
2024-10-02 09:07:27 +1000

    Delete src/main/java/hub/troubleshooters/soundlink/core/events/models/SearchEventModel.java
    This is not needed anymore because all search event code into events code

--- src/main/java/hub/troubleshooters/soundlink/core/events/models/SearchEventModel.java
+++
@@ -1,6 +0,0 @@
-package hub.troubleshooters.soundlink.core.events.models;
-
-import java.util.Date;
-
-public record SearchEventModel(String name, String description, Date scheduledDate, String Venue, int capacity, int communityId) {
-}
