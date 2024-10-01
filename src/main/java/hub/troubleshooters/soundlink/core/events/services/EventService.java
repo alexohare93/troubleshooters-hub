e5f4ebf58739144d35802df16fba5e0eeb756bb2
alexohare93 <109781158+alexohare93@users.noreply.github.com>
2024-10-02 08:59:26 +1000

    Add get user community events and get public community events

--- src/main/java/hub/troubleshooters/soundlink/core/events/services/EventService.java
+++ src/main/java/hub/troubleshooters/soundlink/core/events/services/EventService.java
@@ -6,11 +6,15 @@ import hub.troubleshooters.soundlink.data.models.SearchEvent;
 import hub.troubleshooters.soundlink.data.factories.SearchEventFactory;
 
 import java.util.List;
+import java.sql.SQLException;
 
 /**
  * Contains all the methods involved in event CRUD operations
  */
 public interface EventService {
     ValidationResult createEvent(CreateEventModel model);
+
+    List<Event> getUserCommunityEvents(int userId) throws SQLException;
+    List<Event> getPublicCommunityEvents(int userId) throws SQLException;
 }
 
