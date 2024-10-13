a5c976e1a966b08366d2e42cae9a053a038a613c
IvyWu825 <zhiyingwu825@gmail.com>
2024-10-13 10:59:59 +1000

    fix merge error

--- src/main/java/hub/troubleshooters/soundlink/app/areas/communities/CreateCommunityController.java
+++ src/main/java/hub/troubleshooters/soundlink/app/areas/communities/CreateCommunityController.java
@@ -1,23 +1,4 @@
 package hub.troubleshooters.soundlink.app.areas.communities;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import hub.troubleshooters.soundlink.app.UserDataStore;
-
-
 public class CreateCommunityController {
-
-    @FXML
-    private TextField communityNameField;
-
-    @FXML
-    public void onCreateCommunityButtonPressed() {
-
-        String communityName = "Test Community";
-
-        UserDataStore userDataStore = UserDataStore.getInstance();
-
-        userDataStore.addUserCommunity(communityName);
-
-    }
 }
