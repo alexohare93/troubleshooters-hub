6305531bdf030fd0a9b2708c7d0ca1fbc511a43d
Corey Batchler <coreybatchler7@gmail.com>
2024-10-20 14:55:14 +1000

    more meaningful homepage button

--- src/main/java/hub/troubleshooters/soundlink/app/areas/home/HomeController.java
+++ src/main/java/hub/troubleshooters/soundlink/app/areas/home/HomeController.java
@@ -1,10 +1,22 @@
 package hub.troubleshooters.soundlink.app.areas.home;
 
 import com.google.inject.Inject;
+import hub.troubleshooters.soundlink.app.areas.Routes;
 import hub.troubleshooters.soundlink.app.services.SceneManager;
 import hub.troubleshooters.soundlink.core.auth.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HomeController {
+    private final SceneManager sceneManager;
+
+    @Inject
+    public HomeController(SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+    }
+
+    @FXML
+    protected void onBrowseCommunitiesButtonClick() {
+        sceneManager.navigate(Routes.SEARCH_COMMUNITIES);
+    }
 }
