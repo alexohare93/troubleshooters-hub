c54c7a530a6f58fb5b214967e606f6a545f4a50b
IvyWu825 <zhiyingwu825@gmail.com>
2024-10-20 19:20:22 +1000

    SL-79

---
+++ src/main/java/hub/troubleshooters/soundlink/core/auth/services/BookingService.java
@@ -0,0 +1,14 @@
+package hub.troubleshooters.soundlink.core.auth.services;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BookingService {
+    public List<String> getUpcomingEventsForUser() {
+        return Arrays.asList(
+                //"Rock Music Fest - 19 December 2024",
+                //"Indie Night at The Club - 15 January 2025",
+                //"Jazz Under the Stars - 10 February 2025"
+        );
+    }
+}
