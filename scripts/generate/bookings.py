70881a85fd1e59edeca0bedc96f3ffd86a306fdc
James Dagnall <James.T.Dagnall@gmail.com>
2024-10-20 13:46:28 +1000

    renamed event_attendees to bookings

--- scripts/generate/bookings.py
+++ scripts/generate/bookings.py
@@ -5,8 +5,8 @@ data = []
 for i in range(100):
     data.append([random.randrange(1, 101), random.randrange(1, 102)])
 
-sql = "INSERT INTO EventAttendees (EventId, UserId) VALUES ({eventId}, {userId});\n"
-path = "../mock_data/event_attendees.sql"
+sql = "INSERT INTO Bookings (EventId, UserId) VALUES ({eventId}, {userId});\n"
+path = "../mock_data/bookings.sql"
 
 with open(path, mode="w", newline="", encoding="utf-8") as file:
     for d in data:
